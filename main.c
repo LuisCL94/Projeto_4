04d3ea17630477a4932bba34dc4532b2ac652659
Aline Laureano de Aráujo VIlela <alinelv_laureano@hotmail.com>
2018-11-06 11:26:03 -0200

    Programa com fila de pedidos dando seg fault

--- main.c
+++ main.c
@@ -1,69 +1,183 @@
+/*
+ * O QUE FAZER:
+ * - Montar uma fila de voos e orgorirar ela pelo 'A' e fuel 0
+ * - Pegar hora inicial de simulação e preencher dados iniciais corretamentes
+ * - Na simulação: diminuir a unidade de tempo dos aviões e de talvez outra coisa
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
+#define U_TEMPO 5
+#define FUEL_D 0
+
+
+/*
+ * STRUCTS
+ */
 typedef struct {
     char codigo_de_voo[7];
     char modo_de_voo;
-    int nivel_de_combustivel; 
-} Aviao;
+    int nivel_de_combustivel;
+}Aviao;
 
-int existe(int *valores, int tam, int valor);
-void rand_voos(int *numeros, int quantNumeros);
+typedef struct fila{
+  Aviao *info;
+  struct fila *prox;
+}Fila;
 
-int main() {
+typedef struct {
+    Fila *inicio, *fim;
+}Cabecalho;
+
+
+/*
+ * ASSINATURA DAS FUNÇÕES
+ */
+int existe(int *valores, int tam, int valor); //OK
+void rand_voos(int *numeros, int quantNumeros); //OK
+Fila *cria_aviao(); //OK
+Cabecalho inicia_fila(int NAproximacoes, int NDecolagens, int NVoos);
+void imprime_fila_de_pedidos();
+void simula_voos();
+
+
+/*
+ * FUNÇÃO PRINCIPAL
+ */
+int main(){
 
     srand(time(NULL));
-    
+
     int NAproximacoes = 10 + (rand() % 23); //valor entre 10 e 32
-    int NDecolagens = 10 + (rand() % 23); 
+    int NDecolagens = 10 + (rand() % 23);
     int NVoos = NAproximacoes + NDecolagens;
 
-    // int CombA = (rand() % 13); // valor entre 0 e 12
+    Cabecalho c;
+    c = inicia_fila(NAproximacoes, NDecolagens, NVoos);
 
-    printf("\"Aeroporto Internacional de Brasilia\"\n");
-    printf("Hora Inicial:\n");
-    printf("Fila de Pedidos:\n");
-    printf("NVoos = %d\n", NVoos);
-    printf("NAproximacoes = %d\n", NAproximacoes);
-    printf("NDecolagens = %d\n", NDecolagens);
-    printf("--------------------------------------\n");
-    printf("Listagem de eventos:\n");
-    // printf("CombA = %d\n", CombA);
-
-    char lista_voos[64][7] = {
-        "VG3001", "JJ4404", "LN7001", "TG1501", "GL7602", "TT1010", "AZ1009", "AZ1008", 
-        "AZ1010", "TG1506", "VG3002", "JJ4402", "GL7603", "RL7880", "AL0012", "TT4544", 
-        "TG1505", "VG3003", "JJ4403", "JJ4401", "LN7002", "AZ1002", "AZ1007", "GL7604", 
-        "AZ1006", "TG1503", "AZ1003", "JJ4406", "AZ1001", "LN7003", "AZ1004", "TG1504", 
-        "AZ1005", "TG1502", "GL7601", "TT4500", "RL7801", "JJ4410", "GL7607", "AL0029", 
-        "VV3390", "VV3392", "GF4681", "GF4690", "AZ1020", "JJ4435", "VG3010", "LF0920", 
-        "AZ1065", "LF0978", "RL7867", "TT4502", "GL7645", "LF0932", "JJ4434", "TG1510",
-        "TT1020", "AZ1098", "BA2312", "VG3030", "BA2304", "KL5609", "KL5610", "KL5611"
-    };
- 
-    printf("\n");
-
-    int voos[NVoos];
-    rand_voos(voos, NVoos);
-
-    for(int i=1; i<=NVoos; i++) {
-        if(i==1)
-            printf("APROXIMACOES:\n");
-        if(i==NAproximacoes+1)
-            printf("DECOLAGENS:\n");
-        printf("%s\n", lista_voos[voos[i]]);
-    }
     return 0;
-}
+}/*FIM-main*/
+
+
+/*
+ * ESCOPO DAS FUNÇÕES
+ */
+void simula_voos(){
+
+}/*FIM-simula_voos*/
+
+Cabecalho inicia_fila(int NAproximacoes, int NDecolagens, int NVoos){
+
+  Cabecalho c;
+  Fila *aux;
+  int hours, min, num_aleatorios[NVoos], tipo, cont_A = 0, cont_D = 0;
+
+  //Inicia Cabecalho - 1º item da lista sem a info preenchida
+  c.inicio = cria_aviao();
+  c.fim = c.inicio;
+
+  //Tempo inicial da simulação
+  hours = rand()%24;
+  do{ //Pega min divisivel por 5
+    min = rand()%56;
+  }while(min%5 != 0);
+
+  char lista_voos[64][7] = {
+      "VG3001", "JJ4404", "LN7001", "TG1501", "GL7602", "TT1010", "AZ1009", "AZ1008",
+      "AZ1010", "TG1506", "VG3002", "JJ4402", "GL7603", "RL7880", "AL0012", "TT4544",
+      "TG1505", "VG3003", "JJ4403", "JJ4401", "LN7002", "AZ1002", "AZ1007", "GL7604",
+      "AZ1006", "TG1503", "AZ1003", "JJ4406", "AZ1001", "LN7003", "AZ1004", "TG1504",
+      "AZ1005", "TG1502", "GL7601", "TT4500", "RL7801", "JJ4410", "GL7607", "AL0029",
+      "VV3390", "VV3392", "GF4681", "GF4690", "AZ1020", "JJ4435", "VG3010", "LF0920",
+      "AZ1065", "LF0978", "RL7867", "TT4502", "GL7645", "LF0932", "JJ4434", "TG1510",
+      "TT1020", "AZ1098", "BA2312", "VG3030", "BA2304", "KL5609", "KL5610", "KL5611"
+  };
+
+  //Gera voos aleatorios
+  rand_voos(num_aleatorios, NVoos);
+
+  //Monta fila de avioes
+  for(int i = 0; i < NVoos; i++){
+
+    if(i){ //Caso exista só 1 avião
+      c.fim->prox = cria_aviao();
+      c.fim = c.fim->prox;
+    }
+
+    //Preenche o código aleatório do avião
+    strcpy(c.fim->info->codigo_de_voo, lista_voos[num_aleatorios[i]]);
+    printf("\t\tCOD: %s\n", c.fim->info->codigo_de_voo);
+
+    tipo = rand() % 2;
+    if(tipo == FUEL_D && cont_D < NDecolagens){
+        c.fim->info->modo_de_voo = 'D';
+        cont_D++;
+
+    }else{
+      if(cont_A < NAproximacoes){
+        c.fim->info->modo_de_voo = 'A';
+        c.fim->info->nivel_de_combustivel = rand()%13;
+        cont_A++;
+
+      }else{
+        c.fim->info->modo_de_voo = 'D';
+        cont_D++;
+      }
+    }
+    c.fim->prox = NULL;
+  }/*FIM-for*/
+
+  //Dados iniciais
+  aux = c.inicio;
+  printf("---------------------------------------------------------------------------------\n");
+  printf("Aeroporto Internacional de Brasília\n");
+  printf("Hora Inicial: %dh %dmin\n", hours, min);
+  printf("Fila de Pedidos:");
+
+  while (aux !=NULL) {
+    printf("\n\t[Código do vôo: %s - ", aux->info->codigo_de_voo);
+    if(aux->info->modo_de_voo == 'A'){
+      printf(" P - %d]", aux->info->nivel_de_combustivel);
+    }else{
+      printf(" D - --]");
+    }
+    aux = aux->prox;
+  }
+
+  printf("\nNVoos: %d\n", NVoos);
+  printf("Naproximações: %d\n", NAproximacoes);
+  printf("NDecolagens: %d\n", NDecolagens);
+  printf("---------------------------------------------------------------------------------\n");
+  printf("Listagem de eventos:\n");
+
+
+  printf("\n");
+
+  for(int i=1; i<=NVoos; i++) {
+      if(i==1)
+          printf("APROXIMACOES:\n");
+      if(i==NAproximacoes+1)
+          printf("DECOLAGENS:\n");
+      printf("%s\n", lista_voos[num_aleatorios[i]]);
+  }
+
+  return c;
+}/*FIM-inicia_fila*/
+
+Fila *cria_aviao(){
+  Fila *novo = (Fila*) malloc(sizeof(Fila));
+  return novo;
+}/*FIM-cria_aviao*/
 
 int existe(int *valores, int tam, int valor) {
     for(int i=1; i<=tam; i++){
-        if(valores[i]==valor)
+        if(valores[i] == valor)
             return 1;
     }
     return 0;
-}
+}/*FIM-existe*/
 
 void rand_voos(int *numeros, int quantNumeros){
     int v;
@@ -73,5 +187,5 @@ void rand_voos(int *numeros, int quantNumeros){
             v = (rand() % 64);
         }
         numeros[i] = v;
-    }	
-}
+    }
+}/*FIM-rand_voos*/
